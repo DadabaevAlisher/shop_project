e9f4dc3ec5a10551090459ffd2f59d297120b30f
DadabevAlisher <Dadabave027@gmail.com>
2025-07-23 22:04:21 +0500

    add model Category

--- app/models.py
+++ app/models.py
@@ -2,14 +2,20 @@ from django.db import models
 
 # Create your models here.
 
-class product(models.Model):
-    text = models.CharField(max_length=50, verbose_name="tovar nomi")
+class Category(models.Model):
+    title = models.CharField(max_length=50, verbose_name="kategoriya nomi")
+
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=50, verbose_name="tovar nomi")
     description = models.TextField( verbose_name="tovar haqida")
     price = models.IntegerField(verbose_name="tovar narxi")
     discount_precent = models.PositiveSmallIntegerField(verbose_name="chegirma foizi")
     is_active = models.BooleanField(default=True)
     image = models.ImageField(upload_to='product_images', blank=True, null=True)
     count = models.PositiveIntegerField(default=1)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
 
 
