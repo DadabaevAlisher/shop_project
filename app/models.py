c439bd7d776343be433308336957b5f3a90837cc
DadabevAlisher <Dadabave027@gmail.com>
2025-07-23 21:25:22 +0500

    add model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class product(models.Model):
+    text = models.CharField(max_length=50, verbose_name="tovar nomi")
+    description = models.TextField( verbose_name="tovar haqida")
+    price = models.IntegerField(verbose_name="tovar narxi")
+    discount_precent = models.PositiveSmallIntegerField(verbose_name="chegirma foizi")
+    is_active = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='product_images', blank=True, null=True)
+    count = models.PositiveIntegerField(default=1)
+
+
+
