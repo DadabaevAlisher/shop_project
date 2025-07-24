b31e735e6256438e9abd53881e4930ade8a117a5
DadabevAlisher <Dadabave027@gmail.com>
2025-07-24 17:59:46 +0500

    app name rename product

--- product/migrations/0002_category_rename_text_product_title_product_category.py
+++ product/migrations/0002_category_rename_text_product_title_product_category.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('app', '0001_initial'),
+        ('product', '0001_initial'),
     ]
 
     operations = [
